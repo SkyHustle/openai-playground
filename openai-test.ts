61db7223b9004b250eb9dd13ea1e698d9d5e1b5d
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-20 13:48:18 -0700

    Text to speech

--- openai-test.ts
+++ openai-test.ts
@@ -1,10 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
+import fs from "fs";
+import path from "path";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+const speechFile = path.resolve("./speech.mp3");
 
 async function main() {
     // const completion = await openai.chat.completions.create({
@@ -27,18 +31,27 @@ async function main() {
 
     // console.log(image.data);
 
-    const stream = await openai.chat.completions.create({
-        model: "gpt-4",
-        messages: [{ role: "user", content: "Provide a hundred word parable" }],
-        stream: true,
-    });
+    // const stream = await openai.chat.completions.create({
+    //     model: "gpt-4",
+    //     messages: [{ role: "user", content: "Provide a hundred word parable" }],
+    //     stream: true,
+    // });
 
-    for await (const chunk of stream) {
-        const content = process.stdout.write(
-            chunk.choices[0]?.delta?.content || ""
-        );
-        console.log(content);
-    }
+    // for await (const chunk of stream) {
+    //     const content = process.stdout.write(
+    //         chunk.choices[0]?.delta?.content || ""
+    //     );
+    //     console.log(content);
+    // }
+
+    const mp3 = await openai.audio.speech.create({
+        model: "tts-1",
+        voice: "alloy",
+        input: "Today is a wonderful day to build something people love!",
+    });
+    console.log(speechFile);
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    await fs.promises.writeFile(speechFile, buffer);
 }
 
 main();
