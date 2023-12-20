bbc10aae6b0e53957af7009f7e4f7b662a4b8755
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-20 13:41:42 -0700

    Streaming is cool

--- openai-test.ts
+++ openai-test.ts
@@ -7,25 +7,38 @@ const openai = new OpenAI({
 });
 
 async function main() {
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "You are a helpful assistant." }],
-        model: "gpt-3.5-turbo",
+    // const completion = await openai.chat.completions.create({
+    //     messages: [{ role: "system", content: "You are a helpful assistant." }],
+    //     model: "gpt-3.5-turbo",
+    // });
+
+    // console.log(completion.choices[0]);
+
+    // const embedding = await openai.embeddings.create({
+    //     model: "text-embedding-ada-002",
+    //     input: "The quick brown fox jumped over the lazy dog",
+    // });
+
+    // console.log(embedding);
+
+    // const image = await openai.images.generate({
+    //     prompt: "A cute baby sea otter",
+    // });
+
+    // console.log(image.data);
+
+    const stream = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [{ role: "user", content: "Provide a hundred word parable" }],
+        stream: true,
     });
 
-    console.log(completion.choices[0]);
-
-    const embedding = await openai.embeddings.create({
-        model: "text-embedding-ada-002",
-        input: "The quick brown fox jumped over the lazy dog",
-    });
-
-    console.log(embedding);
-
-    const image = await openai.images.generate({
-        prompt: "A cute baby sea otter",
-    });
-
-    console.log(image.data);
+    for await (const chunk of stream) {
+        const content = process.stdout.write(
+            chunk.choices[0]?.delta?.content || ""
+        );
+        console.log(content);
+    }
 }
 
 main();
