8363758d3d9e030a5872bbb7a80f03017acfda80
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-20 16:42:16 -0700

    Whoa..... automated function calls? Wha......ga .

--- openai-test.ts
+++ openai-test.ts
@@ -15,35 +15,65 @@ async function main() {
     //     messages: [{ role: "system", content: "You are a helpful assistant." }],
     //     model: "gpt-3.5-turbo",
     // });
-
     // console.log(completion.choices[0]);
+
+    const runner = openai.beta.chat.completions
+        .runTools({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "user", content: "How is the weather this week?" },
+            ],
+            tools: [
+                {
+                    type: "function",
+                    function: {
+                        function: getCurrentLocation,
+                        parameters: { type: "object", properties: {} },
+                        description: "Get the user's current location",
+                    },
+                },
+                {
+                    type: "function",
+                    function: {
+                        function: getWeather,
+                        parse: JSON.parse, // or use a validation library like zod for typesafe parsing.
+                        parameters: {
+                            type: "object",
+                            properties: {
+                                location: { type: "string" },
+                            },
+                        },
+                        description: "Get the weather for a location",
+                    },
+                },
+            ],
+        })
+        .on("message", (message) => console.log(message));
+
+    const finalContent = await runner.finalContent();
+    console.log();
+    console.log("Final content:", finalContent);
 
     // const embedding = await openai.embeddings.create({
     //     model: "text-embedding-ada-002",
     //     input: "The quick brown fox jumped over the lazy dog",
     // });
-
     // console.log(embedding);
-
     // const image = await openai.images.generate({
     //     prompt: "A cute baby sea otter",
     // });
-
     // console.log(image.data);
-
     // const stream = await openai.chat.completions.create({
     //     model: "gpt-4",
     //     messages: [{ role: "user", content: "Provide a hundred word parable" }],
     //     stream: true,
     // });
-
     // for await (const chunk of stream) {
     //     const content = process.stdout.write(
     //         chunk.choices[0]?.delta?.content || ""
     //     );
     //     console.log(content);
     // }
-
     // const mp3 = await openai.audio.speech.create({
     //     model: "tts-1",
     //     voice: "alloy",
@@ -52,13 +82,11 @@ async function main() {
     // console.log(speechFile);
     // const buffer = Buffer.from(await mp3.arrayBuffer());
     // await fs.promises.writeFile(speechFile, buffer);
-
     // const transcription = await openai.audio.transcriptions.create({
     //     file: fs.createReadStream("speech.mp3"),
     //     model: "whisper-1",
     // });
     // console.log(transcription.text);
-
     // const myAssistant = await openai.beta.assistants.create({
     //     instructions:
     //         "You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
@@ -67,7 +95,6 @@ async function main() {
     //     model: "gpt-4",
     // });
     // console.log(myAssistant);
-
     // const messageThread = await openai.beta.threads.create({
     //     messages: [
     //         {
@@ -82,10 +109,26 @@ async function main() {
     //     ],
     // });
     // console.log(messageThread);
+    // const myThread = await openai.beta.threads.retrieve("thread_id");
+    // console.log(myThread);
+    // const threadMessages = await openai.beta.threads.messages.create(
+    //     "thread_id",
+    //     {
+    //         role: "user",
+    //         content: "How does AI work? Explain it in simple terms.",
+    //     }
+    // );
+    // console.log(threadMessages);
+}
 
-    const myThread = await openai.beta.threads.retrieve("thread_id");
+async function getCurrentLocation() {
+    return "Boston"; // Simulate lookup
+}
 
-    console.log(myThread);
+async function getWeather(args: { location: string }) {
+    const { location } = args;
+    // … do lookup …
+    return { temperature: "90", precipitation: "10%" };
 }
 
 main();
