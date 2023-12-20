310711dc833bdb2209111c6d25fbf66b86c9c0ba
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-20 12:57:09 -0700

    Basic chat completions, embeddings and image generation

--- openai-test.ts
+++ openai-test.ts
@@ -13,6 +13,19 @@ async function main() {
     });
 
     console.log(completion.choices[0]);
+
+    const embedding = await openai.embeddings.create({
+        model: "text-embedding-ada-002",
+        input: "The quick brown fox jumped over the lazy dog",
+    });
+
+    console.log(embedding);
+
+    const image = await openai.images.generate({
+        prompt: "A cute baby sea otter",
+    });
+
+    console.log(image.data);
 }
 
 main();
